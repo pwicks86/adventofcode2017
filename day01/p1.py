6122013b3d8e902e5d439aca1ac5569abce881ec
Paul Wicks <pwicks86@gmail.com>
2017-12-01 01:01:03 -0800

    Day 1 complete

--- day01/p1.py
+++ day01/p1.py
@@ -1,1 +1,12 @@
 f = open('input.txt')
+instr = f.read()
+num_list = [int(n) for n in instr]
+add_nums = []
+for i in range(len(num_list) - 1):
+    if (num_list[i] == num_list[i+1]):
+        add_nums.append(num_list[i])
+if num_list[0] == num_list[-1]:
+    add_nums.append(num_list[0])
+print(sum(add_nums))
+
+
--- day01/p2.py
+++ day01/p2.py
@@ -1,1 +1,14 @@
 f = open('input.txt')
+instr = f.read()
+num_list = [int(n) for n in instr]
+add_nums = []
+for i in range(len(num_list) - 1):
+    p = (i + (len(num_list) / 2)) % len(num_list)
+    if (num_list[i] == num_list[p]):
+        add_nums.append(num_list[i])
+if num_list[-1] == num_list[(len(num_list) / 2) - 1]:
+    add_nums.append(num_list[-1])
+print(sum(add_nums))
+
+
+
