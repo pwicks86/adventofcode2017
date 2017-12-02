e454027c05d356106c5a722de3e1d153110a3fe5
Paul Wicks <pwicks86@gmail.com>
2017-12-01 23:32:09 -0800

    Day 2 complete

--- day02/p1.py
+++ day02/p1.py
@@ -1,1 +1,11 @@
 f = open('input.txt')
+lines = []
+for l in f.readlines():
+    lines.append(map(int, l.split()))
+checksum = []
+for l in lines:
+    lsorted = sorted(l)
+    checksum.append(abs(lsorted[0] - lsorted[-1]))
+
+print(sum(checksum))
+
--- day02/p2.py
+++ day02/p2.py
@@ -1,1 +1,15 @@
+from itertools import combinations
 f = open('input.txt')
+lines = []
+for l in f.readlines():
+    lines.append(map(int, l.split()))
+n = []
+for l in lines:
+    for c in combinations(l, 2):
+        d = sorted(c)
+        if d[1] % d[0] == 0:
+            n.append(d[1] / d[0])
+            break
+print(sum(n))
+
+
