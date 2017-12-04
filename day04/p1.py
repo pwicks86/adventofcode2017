8433f9740345198a233da2bff390e70095094138
Paul Wicks <pwicks86@gmail.com>
2017-12-03 22:07:56 -0800

    Day 4 complete

--- day04/p1.py
+++ day04/p1.py
@@ -1,1 +1,12 @@
+from collections import Counter
 f = open('input.txt')
+valid = 0
+for l in f.readlines():
+    words = l.split()
+    c = Counter(words)
+    if c.most_common(1)[0][1] == 1:
+        valid += 1
+print(valid)
+
+
+    
--- day04/p2.py
+++ day04/p2.py
@@ -1,1 +1,14 @@
+from collections import Counter
 f = open('input.txt')
+valid = 0
+for l in f.readlines():
+    words = l.split()
+    word_sets = [frozenset(w) for w in words]
+    c = Counter(word_sets)
+    if c.most_common(1)[0][1] == 1:
+        valid += 1
+print(valid)
+
+
+    
+
