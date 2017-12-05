26f7a8a607d09b09afc7ba18fb9c7ddaf97c2d4f
Paul Wicks <pwicks86@gmail.com>
2017-12-04 21:13:09 -0800

    Day 5 complete

--- day05/p1.py
+++ day05/p1.py
@@ -1,1 +1,14 @@
 f = open('input.txt')
+instructions = [int(i) for i in f.readlines()]
+num_inst = len(instructions)
+cur_pos = 0
+steps = -1
+while True:
+    steps += 1
+    try:
+        cur_val = instructions[cur_pos]
+        instructions[cur_pos] += 1
+        cur_pos += cur_val
+    except:
+        print(steps)
+        break
--- day05/p2.py
+++ day05/p2.py
@@ -1,1 +1,19 @@
 f = open('input.txt')
+instructions = [int(i) for i in f.readlines()]
+num_inst = len(instructions)
+cur_pos = 0
+steps = -1
+while True:
+    steps += 1
+    try:
+        cur_val = instructions[cur_pos]
+        if cur_val >= 3:
+            instructions[cur_pos] -= 1
+        else:
+            instructions[cur_pos] += 1
+
+        cur_pos += cur_val
+    except:
+        print(steps)
+        break
+
