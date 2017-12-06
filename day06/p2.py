393696aaf7127d744ab3c43ad80be28182a8151d
Paul Wicks <pwicks86@gmail.com>
2017-12-05 23:30:16 -0800

    day 6, p2 complete

--- day06/p2.py
+++ day06/p2.py
@@ -1,1 +1,22 @@
 f = open('input.txt')
+banks = [int(b) for b in f.read().split()]
+# banks = [0, 2, 7, 0]
+seen = {}
+seen[tuple(banks)] = 1
+cycles = 0
+while True:
+    max_index = banks.index(max(banks))
+    max_val = banks[max_index]
+    banks[max_index] = 0
+    for i in range(max_index + 1, max_index + max_val + 1):
+        pos = i % len(banks)
+        banks[pos] = banks[pos] + 1
+    cycles += 1
+    t = tuple(banks)
+    if t in seen:
+        print(seen[t])
+        break
+    else:
+        for k in seen.keys():
+            seen[k] += 1
+        seen[t] = 1
