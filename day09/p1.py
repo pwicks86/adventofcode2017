7745bd6fe99956fa6058b692271f34eba216a1d7
Paul Wicks <pwicks86@gmail.com>
2017-12-08 22:32:35 -0800

    day 09 complete

--- day09/p1.py
+++ day09/p1.py
@@ -1,1 +1,31 @@
+from collections import deque
 f = open('input.txt')
+stack = deque()
+ignore = False
+score = 0
+s = f.read()
+for c in s:
+    if ignore:
+        ignore = False
+        continue
+    if c == "!":
+        ignore = True
+    elif c == "{":
+        if len(stack) == 0:
+            stack.append(c)
+        elif stack[-1] != "<":
+            stack.append(c)
+    elif c == "}":
+        if stack[-1] == "{":
+            stack.pop()
+            score += 1 + len(stack)
+        elif stack[-1] == "<":
+            pass
+    elif c == "<":
+        if stack[-1] != "<":
+            stack.append(c)
+    elif c == ">":
+        if stack[-1] == "<":
+            stack.pop()
+
+print(score)
--- day09/p2.py
+++ day09/p2.py
@@ -1,1 +1,36 @@
+from collections import deque
 f = open('input.txt')
+stack = deque()
+ignore = False
+garbage = 0
+s = f.read()
+for c in s:
+    if ignore:
+        ignore = False
+        continue
+    if c == "!":
+        ignore = True
+    elif c == "{":
+        if len(stack) == 0:
+            stack.append(c)
+        if stack[-1] == "<":
+            garbage += 1
+    elif c == "}":
+        if stack[-1] == "<":
+            garbage += 1
+    elif c == "<":
+        if len(stack) == 0:
+            stack.append(c)
+        elif len(stack) > 0 and stack[-1] != "<":
+            stack.append(c)
+        elif stack[-1] == "<":
+            garbage += 1
+    elif c == ">":
+        if len(stack) > 0 and stack[-1] == "<":
+            stack.pop()
+    else:
+        if stack[-1] == "<":
+            garbage += 1
+
+print(garbage)
+
