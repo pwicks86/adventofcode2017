1465482507cec7af8d5be21b1663ea2ed5481d30
Paul Wicks <pwicks86@gmail.com>
2017-12-09 21:27:45 -0800

    day 10 p1 complete

--- day10/p1.py
+++ day10/p1.py
@@ -1,1 +1,20 @@
 f = open('input.txt')
+skip_size = 0
+cur_pos = 0
+lens = map(int, f.read().split(","))
+nums = list(range(256))
+for l in lens:
+    start = cur_pos
+    end = start + l
+    # reverse
+    for i in range(l/2):
+        b = (end - i - 1) % len(nums)
+        a = (start + i) % len(nums)
+        temp = nums[a]
+        nums[a] = nums[b]
+        nums[b] = temp
+    # adjust cur
+    cur_pos = cur_pos + l + skip_size
+    skip_size += 1
+
+print(nums[0] * nums[1])
