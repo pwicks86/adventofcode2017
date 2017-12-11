ba0016eaac269bd4c65b49a37e1be4affd8d0cb6
Paul Wicks <pwicks86@gmail.com>
2017-12-10 21:32:18 -0800

    day 11 complete

--- day11/p1.py
+++ day11/p1.py
@@ -1,1 +1,26 @@
 f = open('input.txt')
+steps = f.read().split(",")
+x = 0
+y = 0
+z = 0
+for s in steps:
+    if s == "n":
+        y += 1
+        z -= 1
+    elif s == "s":
+        y -= 1
+        z += 1
+    elif s == "nw":
+        y += 1
+        x -= 1
+    elif s == "ne":
+        x += 1
+        z -= 1
+    elif s == "se":
+        y -= 1
+        x += 1
+    elif s == "sw":
+        z += 1
+        x -= 1
+    
+print(max(x,y,z))
