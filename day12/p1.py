548a545b2e1c4a6093f25911433a6e073ec3507e
Paul Wicks <pwicks86@gmail.com>
2017-12-11 21:19:55 -0800

    day 12 p1 complete

--- day12/p1.py
+++ day12/p1.py
@@ -1,1 +1,29 @@
+import re
 f = open('input.txt')
+zero_set = set()
+lines = f.readlines()
+def add_line(line):
+    from_node, to_nodes = line.split("<->")
+    to_node_list = to_nodes.split(",")
+    zero_set.add(from_node.strip())
+    zero_set.update([n.strip() for n in to_node_list])
+
+line_one = lines.pop(0)
+add_line(line_one)
+zero_size = -1
+while True:
+    remove_line = None
+    for l in lines:
+        from_node, to_nodes = l.split("<->")
+        if from_node.strip() in zero_set:
+            add_line(l)
+            remove_line = l
+            break
+    if remove_line:
+        lines.remove(remove_line)
+    else:
+        break
+
+print(len(zero_set))
+
+
