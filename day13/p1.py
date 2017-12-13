b7303e8c249d15f1f4a214f862f621fe069bd56d
Paul Wicks <pwicks86@gmail.com>
2017-12-12 21:26:55 -0800

    Day 13, p1 complete

--- day13/p1.py
+++ day13/p1.py
@@ -1,1 +1,13 @@
 f = open('input.txt')
+wall = {}
+depths = {}
+for l in f.readlines():
+    layer, depth = [int(x.strip()) for x in l.split(":")]
+    wall[layer] = 1 if depth == 1 else (depth - 1) * 2
+    depths[layer] = depth
+num_steps = max(wall.keys())
+sev = 0
+for s in range(0, num_steps + 1):
+    if s in wall and s % wall[s] == 0:
+        sev += s * depths[s]
+print(sev)
