0a921fc2323ef625c5a9c16520375d343e5bc14e
Paul Wicks <pwicks86@gmail.com>
2017-12-14 22:06:44 -0800

    Day 15, p1 complete

--- day15/p1.py
+++ day15/p1.py
@@ -1,1 +1,17 @@
 f = open('input.txt')
+a = int(f.readline().split(" ")[-1])
+b = int(f.readline().split(" ")[-1])
+a_factor = 16807
+b_factor = 48271
+divider = 2147483647
+
+def gen_val(v, factor):
+    return (v * factor) % divider
+
+count = 0
+for i in range(40000000):
+    a = gen_val(a, a_factor)
+    b = gen_val(b, b_factor)
+    if ((a & 0xFFFF) == (b & 0xFFFF)):
+        count += 1
+print(count)
