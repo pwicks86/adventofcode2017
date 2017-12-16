6434b1dc9319ddf58d9cd9ede9b8b65c05578707
Paul Wicks <pwicks86@gmail.com>
2017-12-15 23:10:51 -0800

    day 16 complete

--- day16/p1.py
+++ day16/p1.py
@@ -1,1 +1,21 @@
+from collections import deque
 f = open('input.txt')
+steps = f.read().split(",")
+progs = deque([chr(o) for o in range(ord("a"), ord("a") + 16)])
+for s in steps:
+    if s[0] == "s":
+        progs.rotate(int(s[1:]))
+    elif s[0] == "x":
+        a, b = map(int, s[1:].split("/"))
+        tmp = progs[a]
+        progs[a] = progs[b]
+        progs[b] = tmp
+    elif s[0] == "p":
+        a_n, b_n = s[1:].split("/")
+        a = progs.index(a_n)
+        b = progs.index(b_n)
+        tmp = progs[a]
+        progs[a] = progs[b]
+        progs[b] = tmp
+
+print("".join(progs))
