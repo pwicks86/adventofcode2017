f49cf6682e4f9f9cb0d90c5257da97fd92f15baa
Paul Wicks <pwicks86@gmail.com>
2017-12-16 21:45:29 -0800

    Day 17 complete

--- day17/p1.py
+++ day17/p1.py
@@ -1,1 +1,9 @@
-f = open('input.txt')
+puz = 377
+buf = [0]
+pos = 0
+for i in range(1, 2017 + 1):
+    pos = (pos + puz) % len(buf)
+    buf.insert(pos + 1, i)
+    pos = (pos + 1) % len(buf)
+x = buf.index(2017)
+print(buf[x+1])
--- day17/p2.py
+++ day17/p2.py
@@ -1,1 +1,14 @@
-f = open('input.txt')
+puz = 377
+pos = 0
+zero_pos = 0
+buflen = 1
+after_zero = -1
+for i in range(1, 50000000 + 1):
+    pos = (pos + puz) % buflen
+    if pos < zero_pos or pos == buflen:
+        zero_pos += 1
+    if pos == zero_pos:
+        after_zero = i
+    buflen += 1
+    pos = (pos + 1) % buflen
+print(after_zero)
